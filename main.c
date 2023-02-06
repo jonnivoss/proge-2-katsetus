4cf061348485a9a6a9d307b3166083079fdce8b5
jonnivoss <92386111+jonnivoss@users.noreply.github.com>
2023-02-06 16:37:19 +0200

    Add files via upload
    uuendus

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
-typedef struct kanditaat {
+typedef struct {
     char nimi[20];
     char erakond[32];
     int haal;
-};
+}kanditaat;
+
+int sisestus(kanditaat *osaleja){
+    printf("sisesta nimi");
+    scanf("%s",osaleja->nimi);
+    printf("sisesta erakond");
+    scanf("%s",osaleja->erakond);
+    printf("sisesta haalte arv");
+    scanf("%d", &osaleja->haal);
+    if(osaleja->haal < 10){
+        return 1;
+    }
+    return 0;
+}
 
 int main() {
-
+    kanditaat kanditaadid[3];
+    for (int i = 0; i < 3; ++i) {
+        sisestus(&kanditaadid[i]);
+    }
 
 
     return 0;
